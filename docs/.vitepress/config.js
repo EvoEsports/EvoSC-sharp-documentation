3164f9356ffe638e08d681c80fe3ef7f8fbedc99
Seliaste <aena.aria@protonmail.com>
2022-07-19 14:43:10 +0200

    improved theme, added social links

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,7 +1,9 @@
 export default {
-  title: "EvoSC# Docs",
+  lang: 'en-US',
+  title: "EvoSC Docs",
   description: "Documentation for EvoSC#.",
   themeConfig: {
+    logo: '/evo.png',
     sidebar: {
       "/devs/": [
         {
@@ -14,6 +16,21 @@ export default {
       { text: "EvoSC# Users", link: "/users/" },
       { text: "Module Developpers", link: "/modudev/" },
       { text: "EvoSC# Developpers", link: "/devs/" },
+    ],
+    footer: {
+      message: 'Made with ❤️ by EvoSC# Devs',
+      copyright: 'Copyright © 2022 Evo'
+    },
+    editLink: {
+      pattern: 'https://github.com/EvoTM/EvoSC-Sharp-Documentation/master/docs/:path',
+      text: 'Edit this page on GitHub'
+    },
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/EvoTM/EvoSC-Sharp' },
+      { icon: 'twitter', link: 'https://twitter.com/EvoTM' },
+      { icon: 'facebook', link: 'https://facebook.com/EvoTrackmania'},
+      { icon: 'discord', link: 'https://discord.gg/evotm' },
+      { icon: 'youtube', link: 'https://youtube.com/c/EvoTrackmania'}
     ],
   },
 };
--- docs/index.md
+++ docs/index.md
@@ -1,2 +1,16 @@
-# EvoSC# Documentation
-
+---
+layout: home
+hero:
+  name: EvoSC# Documentation
+  text: A server controller for everyone
+  tagline: EvoSC# is the new iteration of the Evo server controllers. 
+  image:
+    src: /evo.png
+  actions:
+    - theme: brand
+      text: Get started with EvoSC#
+      link: /users/index
+    - theme: alt
+      text: View on GitHub
+      link: https://github.com/EvoTM/EvoSC-sharp
+---
